f5d86493c25f21632bf3249d2b847b24df25bdbb
Lucas Clazer <lucas.perussolo@evoluum.com.br>
2021-05-07 00:20:31 -0300

    Creating dual theme support.

--- styled-components-react-app/src/App.js
+++ styled-components-react-app/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { temaEscuro } from "./Components/UI/temas";
 function App() {
   return (
     <>
+      <ThemeProvider theme={temaEscuro}>
+
       <GlobalStyle />
       <Cabecalho />
       <Container />
+      </ThemeProvider>
     </>
   );
 }
---
+++ styled-components-react-app/src/Components/UI/temas.js
@@ -0,0 +1,20 @@
+import {
+  fundoClaro,
+  textoFundoClaro,
+  conteudoClaro,
+  fundEscuro,
+  conteudoEscuro,
+  textoFundoEscuro,
+} from "./variables";
+
+export const temaClaro = {
+  body: fundoClaro,
+  inside: conteudoClaro,
+  text: textoFundoClaro,
+};
+
+export const temaEscuro = {
+  body: fundEscuro,
+  inside: conteudoEscuro,
+  text: textoFundoEscuro,
+};

fb79642e0a91f7f82c2dfb0c3cd20a7e5a7a7f30
Lucas Clazer <lucas.perussolo@evoluum.com.br>
2021-05-07 00:27:34 -0300

    Adding support for dark theme.

--- styled-components-react-app/src/Components/Container/index.jsx
+++ styled-components-react-app/src/Components/Container/index.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 import styled from "styled-components";
-import { fundoClaro } from "../UI/variables";
 import Extrato from "../Extrato";
 
 const Container = styled.div`
-  background-color: ${fundoClaro};
+  background-color: ${({ theme }) => theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
 `;
@@ -15,6 +14,8 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  color: ${({ theme }) => theme.text};
+
   @media (max-width: 800px) {
     flex-direction: column;
   }

508028d1a648c03dae85f7d92a777ead2b655b3d
Lucas Clazer <lucas.perussolo@evoluum.com.br>
2021-05-16 16:09:09 -0300

    Testing especific function.

--- automotion-test-react-app/src/App.test.js
+++ automotion-test-react-app/src/App.test.js
@@ -1,28 +1,44 @@
 
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import App from './App';
+import App, { calcularNovoSaldo } from './App';
 
 
 describe("Test Main app.js", () => {
   describe('On app start', () => {
-    it("check bank name", () => {
-    //renders the component
+    test("check bank name", () => {
+      //renders the component
       render(<App />);
       //search into the componente for the specfic text.
       expect(screen.getByText("ByteBank")).toBeInTheDocument();
     });
-    
+
     it('check Show Balance text.', () => {
-      render(<App/>);
+      render(<App />);
       expect(screen.getByText('Saldo:')).toBeInTheDocument();
     })
-  
+
     it('check Transaction button', () => {
-      render(<App/>);
+      render(<App />);
       expect(screen.getByText('Realizar operação')).toBeInTheDocument();
     })
   })
-  
+
+
+  describe('When transaction was done', () => {
+    //testing specific function
+    it('check withdraw transaction', () => {
+      const values = {
+        transacao: 'saque',
+        valor: 50
+      }
+
+      const newBalance = calcularNovoSaldo(values, 150);
+
+      expect(newBalance).toBe(100);
+
+    });
+  })
+
 });
 

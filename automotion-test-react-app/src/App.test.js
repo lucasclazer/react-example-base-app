4e303e472c1846220313de65e6a8d2d4765ab55e
Lucas Clazer <lucas.perussolo@evoluum.com.br>
2021-05-16 15:35:39 -0300

    Sample tests examples.

--- automotion-test-react-app/src/App.test.js
+++ automotion-test-react-app/src/App.test.js
@@ -4,12 +4,25 @@ import React from 'react';
 import App from './App';
 
 
-
 describe("Test Main app.js", () => {
-  it("Testing bank name", () => {
-    render(<App />);
-    expect(screen.getByText("ByteBank")).toBeInTheDocument();
-  });
+  describe('On app start', () => {
+    it("check bank name", () => {
+    //renders the component
+      render(<App />);
+      //search into the componente for the specfic text.
+      expect(screen.getByText("ByteBank")).toBeInTheDocument();
+    });
+    
+    it('check Show Balance text.', () => {
+      render(<App/>);
+      expect(screen.getByText('Saldo:')).toBeInTheDocument();
+    })
+  
+    it('check Transaction button', () => {
+      render(<App/>);
+      expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+    })
+  })
+  
 });
-
 
